17650cbab64dde6fdf270c2fbf04818dc65f8961
jposo <jposorniot@gmail.com>
2026-01-15 12:18:08 -0800

    add server hook

---
+++ src/hooks.server.ts
@@ -0,0 +1,33 @@
+import type { Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({event, resolve}) => {
+    const response = await resolve(event);
+    const supabaseStorageUrl = "";
+
+    // security headers
+    response.headers.set("X-Content-Type-Options", "nosniff");
+    response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+
+    // set permissions policy, empty = disabled
+    response.headers.set("Permissions-Policty",
+        "geolocation=(), microphone=(), camera=(), fullscreen=(self), payment=(), usb=()"
+    );
+
+    response.headers.set("Content-Security-Policy", 
+        "default-src 'self'; " + 
+        "script-src 'self' 'unsafe-inline'; " + 
+        "style-src 'self' 'unsafe-inline'; " +
+        `img-src 'self' ${supabaseStorageUrl}; ` + 
+        "font-src 'self'; " +
+        `connect-src 'self' ${supabaseStorageUrl}; ` +
+        ""
+    );
+
+    if (event.url.protocol === "https:") {
+        response.headers.set("Strict-Transport-Security",
+            "max-age=31536000; includeSubDomains; preload"
+        );
+    }
+
+    return response;
+}
