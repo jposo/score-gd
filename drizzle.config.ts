0f8bba119ffe06501967f478e190a40eb0112cac
jposo <jposorniot@gmail.com>
2026-07-01 18:06:29 -0700

    migrate helper script for production deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "drizzle-kit";
 
 const dbUrl = Deno.env.get("DATABASE_URL");
 if (!dbUrl) {
-  throw new Error("DATABASE_URL is not defined");
+    throw new Error("DATABASE_URL is not defined");
 }
 
 export default defineConfig({
-  schema: "./src/lib/server/db/schema.ts",
-  out: "./supabase/migrations",
-  dialect: "postgresql",
-  dbCredentials: { url: dbUrl },
-  verbose: true,
-  strict: true,
+    schema: "./src/lib/server/db/schema.ts",
+    out: "./supabase/migrations",
+    dialect: "postgresql",
+    dbCredentials: { url: dbUrl },
+    verbose: true,
+    strict: true,
 });
